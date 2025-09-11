749e09388666486a2de1ab391e8229b3ffe21b1c
Srineel <srineelx23@gmail.com>
2025-09-11 11:02:25 +0530

    test changes for ngrok commit

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className='' >
         <div className='glass-card w-75 mx-auto mt-5'>
-            <h1 className='text-center'>Welcome to TYB<br /></h1>
+            <h1 className='text-center'>Welcome to TYB Jordan Belfort<br /></h1>
             <p className='text-center lead fs-3 fw-bold'>A simple and easy way to track your budget !</p>
         </div>
     </div>
